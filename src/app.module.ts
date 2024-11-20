72779a2e311373e84e306979ca6d27774d0f5da5
Wico Tang <wicotang@gmail.com>
2024-11-20 16:21:56 +0800

    feat: module Rent: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RentModule } from './rent/rent.module';
 
 @Module({
-  imports: [],
+  imports: [RentModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/rent/rent.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RentController } from './rent.controller';
+import { RentService } from './rent.service';
+
+@Module({
+  controllers: [RentController],
+  providers: [RentService],
+})
+export class RentModule {}
