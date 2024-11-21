20b7bd99ccb2364153185d3f297f35e76818605c
Wico Tang <wicotang@gmail.com>
2024-11-21 16:31:07 +0800

    feat: module Users: init & entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+
 import { RentModule } from "./rent/rent.module";
 
+import { User } from "./users/user.entity";
+import { UsersModule } from "./users/users.module";
+
 @Module({
-	imports: [RentModule],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: process.env.NODE_ENV === "production" ? ".env.production.local" : ".env.local",
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRoot({
+			type: "postgres",
+			host: process.env.POSTGRES_HOST,
+			port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			entities: [User],
+			synchronize: process.env.NODE_ENV === "development" ? true : false,
+		}),
+		RentModule,
+		UsersModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
