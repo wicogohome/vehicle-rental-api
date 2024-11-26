29a795856ec3ef1fcde815e692be66ac631d2f24
Wico Tang <wicotang@gmail.com>
2024-11-26 12:39:50 +0800

    docs: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder().setTitle("Vehicle Rental API").setDescription("").setVersion("1.0").build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api-doc", app, documentFactory);
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
